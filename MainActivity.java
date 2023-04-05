0bd394c28a090c64708552f53cca342fa8151192
Johnny Sins <128035458+LoneMister@users.noreply.github.com>
2023-04-05 20:52:39 +0300

    Add files via upload

---
+++ MainActivity.java
@@ -0,0 +1,120 @@
+package com.app.loginapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.content.SharedPreferences;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.StringRequest;
+import com.android.volley.toolbox.Volley;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class MainActivity extends AppCompatActivity {
+
+    TextView textViewName, textViewEmail, textViewFetchResult;
+    SharedPreferences sharedPreferences;
+    Button buttonLogout, buttonFetchUser;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        textViewName = findViewById(R.id.name);
+        textViewEmail = findViewById(R.id.email);
+        textViewFetchResult = findViewById(R.id.fetchResult);
+        buttonLogout = findViewById(R.id.logout);
+        buttonFetchUser = findViewById(R.id.fetchProfile);
+        sharedPreferences = getSharedPreferences("MyAppName", MODE_PRIVATE);
+        if (sharedPreferences.getString("logged", "false").equals("false")) {
+            Intent intent = new Intent(getApplicationContext(), LoginActivity.class);
+            startActivity(intent);
+            finish();
+        }
+        textViewName.setText(sharedPreferences.getString("name",""));
+        textViewEmail.setText(sharedPreferences.getString("email",""));
+        buttonLogout.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                RequestQueue queue = Volley.newRequestQueue(getApplicationContext());
+                String url = "https://192.168.43.24/login-registration-android/logout.php";
+
+                StringRequest stringRequest = new StringRequest(Request.Method.POST, url,
+                        new Response.Listener<String>() {
+                            @Override
+                            public void onResponse(String response) {
+
+
+                                if(response.equals("success")) {
+                                    SharedPreferences.Editor editor = sharedPreferences.edit();
+                                    editor.putString("logged", "");
+                                    editor.putString("name", "");
+                                    editor.putString("email", "");
+                                    editor.apply();
+                                    Intent intent = new Intent(getApplicationContext(), MainActivity.class);
+                                    startActivity(intent);
+                                    finish();
+                                }else{
+                                    Toast.makeText(MainActivity.this,response,Toast.LENGTH_SHORT).show();
+                                }
+                            }
+                        }, new Response.ErrorListener() {
+                    @Override
+                    public void onErrorResponse(VolleyError error) {
+                        error.printStackTrace();
+                    }
+                }) {
+                    protected Map<String, String> getParams() {
+                        Map<String, String> paramV = new HashMap<>();
+                        paramV.put("email", sharedPreferences.getString("email",""));
+                        paramV.put("apiKey", sharedPreferences.getString("apiKey",""));
+                        return paramV;
+                    }
+                };
+                queue.add(stringRequest);
+            }
+        });
+        buttonFetchUser.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                RequestQueue queue = Volley.newRequestQueue(getApplicationContext());
+                String url = "https://192.168.43.24/login-registration-android/profile.php";
+
+                StringRequest stringRequest = new StringRequest(Request.Method.POST, url,
+                        new Response.Listener<String>() {
+                            @Override
+                            public void onResponse(String response) {
+
+                                textViewFetchResult.setText(response);
+                                textViewFetchResult.setVisibility(View.VISIBLE);
+                            }
+                        }, new Response.ErrorListener() {
+                    @Override
+                    public void onErrorResponse(VolleyError error) {
+                        error.printStackTrace();
+                    }
+                }) {
+                    protected Map<String, String> getParams() {
+                        Map<String, String> paramV = new HashMap<>();
+                        paramV.put("email", sharedPreferences.getString("email",""));
+                        return paramV;
+                    }
+                };
+                queue.add(stringRequest);
+            }
+        });
+    }
+}
